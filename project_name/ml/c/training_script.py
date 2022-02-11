4176a2fc23f23ccd37659fe559b9882a9ccbde1b
Jason McKeever <jasonmckeever@fakemail.com>
2022-02-11 16:51:41 -0600

    Updated for new features and to use fma small by default.

--- project_name/ml/c/training_script.py
+++ project_name/ml/c/training_script.py
@@ -3,39 +3,58 @@ import time
 
 import tensorflow as tf
 
-from project_name.ml.c.ml_algo_c import MlAlgoC
-
 import project_name.data.data_loader as dl
 import project_name.data.feature_recorder as fr
+from project_name.ml.c.ml_algo_c import MlAlgoC
 
+# Are we loading or training from a fresh state?
 LOAD_ALGO = False
+# Are we using the resnet algo_c variant?
+USE_RESNET = True
 
 if __name__ == '__main__':
 
     start_time = time.time()
 
-    data_type = 'gtzan'
+    data_type = 'fma'
 
     if len(sys.argv) > 1:
         data_type = sys.argv[1]
     if len(sys.argv) > 2:
         fma_set = sys.argv[2]
 
+    # Used to gather up all the files and associate them with splits and
+    # labels.
     loader = dl.DataLoader(data_type=data_type)
+    # Used to load features from .tfrecord files
     recorder = fr.FeatureRecorder()
 
-    file_paths = loader.gather_data('.c.tfrecord', include_labels=False)
+    # Get filenames, labels, and splits
+    metadata = loader.gather_data('.c.tfrecord', include_labels=False)
+    # print(metadata.shape)
+    # print(metadata)
 
-    # Get the splits
-    training, validation, testing = MlAlgoC.split_dataframe_gtzan(file_paths)
+    # Separate the splits
+    training = metadata[metadata['split'] == 'training']
+    validation = metadata[metadata['split'] == 'validation']
+    test = metadata[metadata['split'] == 'test']
+    # Debug --- REMOVE LATER
+    print('Splits:')
+    print(training.shape)
+    print(validation.shape)
+    print(test.shape)
+
     # Create smart tensorflow dataset objects that can load the data on demand
     training = MlAlgoC.create_dataset(training)
     validation = MlAlgoC.create_dataset(validation)
-    testing = MlAlgoC.create_dataset(testing)
+    test = MlAlgoC.create_dataset(test)
 
-    # Get previously trained algo to train it some more!
+    # Get previously trained algo to train it some more.
     if LOAD_ALGO:
-        ml_algo = MlAlgoC(load_path=MlAlgoC.MODEL_PATH)
+        if USE_RESNET:
+            ml_algo = MlAlgoC(load_path=MlAlgoC.MODEL_RESNET_PATH)
+        else:
+            ml_algo = MlAlgoC(load_path=MlAlgoC.MODEL_PATH)
     # Build the algo model from scratch and train it
     else:
         # We have to retrieve 1 element (acutally 1 batch) to inspect the data
@@ -43,13 +62,20 @@ if __name__ == '__main__':
         for elem in training.take(1):
             input_shape = tf.shape(elem[0])[1:].numpy()
             label_shape = tf.shape(elem[1])[1:].numpy()
-            print(input_shape)
-        ml_algo = MlAlgoC(input_shape=input_shape, output_shape=label_shape)
+            # Debug --- REMOVE LATER
+            print('input shape:')
+            print(tf.shape(elem[0]).numpy())
+        ml_algo = MlAlgoC(input_shape=input_shape, output_shape=label_shape,
+                          use_resnet=USE_RESNET)
 
     # Compile the model and fit the training data
     ml_algo.compile_model()
     ml_algo.model.fit(training, epochs=50, validation_data=validation)
     # Evaluate performance on the test set
-    ml_algo.model.evaluate(testing)
+    ml_algo.model.evaluate(test)
+
     # Save our trained algo for future usage
-    ml_algo.save_model()
+    if USE_RESNET:
+        ml_algo.save_model(path=MlAlgoC.MODEL_RESNET_PATH)
+    else:
+        ml_algo.save_model(path=MlAlgoC.MODEL_PATH)
