4fc82618af37a1787d6dbbd317e3139e66794013
Jason McKeever <jasonmckeever@fakemail.com>
2022-02-11 16:47:26 -0600

    Made it work better with the fma dataset. Made both gather_data variants return a 'split' column with assignment of training, validation, or test.

--- project_name/data/data_loader.py
+++ project_name/data/data_loader.py
@@ -10,6 +10,11 @@ class DataLoader:
     DATA_DIR = __file__ + '/../../resources'
     DATA_TYPE = 'gtzan'
     FMA_SET = 'small'
+    # Training / validation / test split.
+    # Out of 100 so we can work with integers.
+    # If you don't need both validation and test, just add the two sets
+    # together later.
+    SPLIT_VALS = [75, 15, 10]
 
     def __init__(self,
                  data_type=DATA_TYPE,
@@ -66,14 +71,21 @@ class DataLoader:
             list(self.gtzan_dir.rglob(f'*{file_extension}')))
 
         if include_labels:
-            genre_labels = [self.get_label(
-                self.genre_dict[x.parent.name]) for x in file_paths]
-            data = pd.DataFrame({'data': file_paths, 'label': genre_labels})
+            genre_labels = [
+                self.get_label(self.genre_dict[x.parent.name])
+                for x in file_paths
+            ]
+            data = pd.DataFrame({'filename': file_paths,
+                                 'label': genre_labels})
         else:
-            data = pd.DataFrame({'data': file_paths})
+            data = pd.DataFrame({'filename': file_paths})
+
+        # Assign splits based on each item's index and add
+        # them to the dataframe as a new column
+        data['split'] = data.index.map(self._assign_split)
 
         # Change the Path objects to strings
-        data['data'] = data['data'].map(str)
+        data['filename'] = data['filename'].map(str)
 
         return data
 
@@ -84,7 +96,7 @@ class DataLoader:
         tracks = pd.read_csv(self.fma_meta_dir / 'tracks.csv',
                              index_col=0,
                              header=1,
-                             usecols=[0, 32, 40, 41, 42],
+                             usecols=[0, 31, 32, 40, 41, 42],
                              skiprows=[2])
         tracks = tracks[tracks['subset'] == self.fma_set]
         # Prune tracks with invalid genre entries
@@ -93,32 +105,59 @@ class DataLoader:
         # Treat the index column as a real data column
         tracks = tracks.reset_index()
         # Select only the columns we care about
-        tracks = tracks[['index', 'genre_top']]
+        tracks = tracks[['index', 'genre_top', 'split']]
 
-        # Map the track ids to their associated filenames
-        tracks['index'] = tracks['index'].map(self._get_filename_fma)
-        file_extender_str = '{{}}.{}'.format(file_extension)
-        tracks['index'] = tracks['index'].map(file_extender_str.format)
-        # More appropriate column names
-        tracks = tracks.rename({'index': 'data', 'genre_top': 'label'}, axis=1)
+        # Map the track ids to their associated filenames as Path objects,
+        # including the file_extension being sought.
+        tracks['index'] = tracks['index'].map(
+            lambda x:
+            self._get_filename_fma(x, file_extension)
+        )
+        # Set more appropriate column names
+        tracks = tracks.rename({'index': 'filename',
+                                'genre_top': 'label'}, axis=1)
 
         if include_labels:
             # Map the genres to their associated labels, ready for ML
             tracks['label'] = tracks['label'].map(self.genre_dict)
             tracks['label'] = tracks['label'].map(self.get_label)
         else:
-            tracks = tracks[['data']]
+            tracks = tracks.loc[:, tracks.columns != 'label']
+
+        # Remove any entries that don't represent real, present files
+        tracks = tracks.loc[tracks['filename'].apply(lambda x: x.is_file())]
 
         # Change the Path objects to strings
-        tracks['data'] = tracks['data'].map(str)
+        tracks['filename'] = tracks['filename'].map(str)
 
         return tracks
 
-    def _get_filename_fma(self, file_id):
+    def _assign_split(self, id):
         """
         """
+        split = ''
+
+        # Assign split based on define split ratio
+        if id % 100 < self.SPLIT_VALS[0]:
+            split = 'training'
+        elif id % 100 < sum(self.SPLIT_VALS[:2]):
+            split = 'validation'
+        elif id % 100 < sum(self.SPLIT_VALS[:3]):
+            split = 'test'
+
+        return split
+
+    def _get_filename_fma(self, file_id, file_extension):
+        """
+        """
+        # Get the folder and filename based on the song ID
         file_id_str = '{:06d}'.format(file_id)
-        return self.fma_dir / file_id_str[:3] / file_id_str
+        # Get the full file path based on where the data is stored, appending
+        # the folder and filename
+        path = self.fma_dir / file_id_str[:3] / file_id_str
+        # Add the file extension
+        path = path.with_suffix(file_extension)
+        return path
 
     def get_label(self, genre_id):
         """
