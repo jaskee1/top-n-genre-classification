c10c02dff84fbbf4fc5c4b953947c028b4e92b96
Jason McKeever <jasonmckeever@fakemail.com>
2022-02-11 16:23:10 -0600

    Implemented a version of Resnet-34 as another optional model architecture.

--- project_name/ml/c/ml_algo_c.py
+++ project_name/ml/c/ml_algo_c.py
@@ -10,14 +10,20 @@ class MlAlgoC:
     """
     """
     MODEL_PATH = Path(__file__ + '/../algo_c_model.h5')
+    MODEL_RESNET_PATH = Path(__file__ + '/../algo_c_resnet_model.h5')
 
     def __init__(self, load_path=None,
                  input_shape=(64, 1288, 1),
-                 output_shape=(10)):
+                 output_shape=(10),
+                 use_resnet=True):
         """
         """
         if load_path is None:
-            self.model = self._build_model(input_shape, output_shape)
+            if use_resnet:
+                self.model = self._build_model_resnet_34(input_shape,
+                                                         output_shape)
+            else:
+                self.model = self._build_model(input_shape, output_shape)
         else:
             self.load_model(load_path)
 
@@ -31,7 +37,7 @@ class MlAlgoC:
         """
         recorder = fr.FeatureRecorder()
         # Get bytes from tfrecord files from the input dataframe
-        dataset = tf.data.TFRecordDataset(dataframe['data'])
+        dataset = tf.data.TFRecordDataset(dataframe['filename'])
         # Map the raw bytes to the properly parsed data
         dataset = dataset.map(recorder.read_tfrecord_from_tfrecord_dataset,
                               num_parallel_calls=n_parse_threads)
@@ -41,6 +47,10 @@ class MlAlgoC:
         # only 1 value in the color channel.
         dataset = dataset.map(lambda x, y:
                               (tf.expand_dims(x, axis=-1), y))
+
+        # Cache the dataset so it doesn't have to prepared again for
+        # every epoch
+        # dataset = dataset.cache()
         # Apply shuffling for order randomization
         dataset = dataset.shuffle(shuffle_buffer_size)
         # Get batches, with prefetching for the next batch
@@ -62,12 +72,6 @@ class MlAlgoC:
     def _build_model(self, input_shape, output_shape):
         """
         """
-        # model = keras.models.Sequential()
-        # model.add(keras.layers.Flatten(input_shape=[64, 1288]))
-        # model.add(keras.layers.Dense(300, activation='relu'))
-        # model.add(keras.layers.Dense(100, activation='relu'))
-        # model.add(keras.layers.Dense(10, activation='softmax'))
-
         model = keras.models.Sequential([
             keras.Input(shape=input_shape),
             keras.layers.BatchNormalization(),
@@ -96,6 +100,47 @@ class MlAlgoC:
 
         return model
 
+    # Code source (adapted from):
+    # Hands on Machine Learning with Scikit-Learn, Keras & Tensorflow
+    # by Aurélien Géron
+    # Chapter 14, page 478-479
+    def _build_model_resnet_34(self, input_shape, output_shape):
+        """
+        """
+        model = keras.models.Sequential()
+        # Input and normalization since I'm not doing external normalization
+        # of the input data
+        model.add(keras.Input(shape=input_shape))
+        model.add(keras.layers.BatchNormalization())
+        # Starting layer
+        model.add(keras.layers.Conv2D(64, 7, strides=2,
+                                      padding='same', use_bias=False))
+        model.add(keras.layers.BatchNormalization())
+        model.add(keras.layers.Activation('relu'))
+        model.add(keras.layers.MaxPool2D(pool_size=3, strides=2,
+                                         padding='same'))
+
+        # Repeated Residual Unit layers
+        # Note that each Residual Unit has 2 main convolutional layers
+        prev_filters = 64
+        for filters in [64] * 3 + [128] * 4 + [256] * 6 + [512] * 3:
+            strides = 1 if filters == prev_filters else 2
+            model.add(ResidualUnit(filters, strides=strides))
+            prev_filters = filters
+
+        # Final fully connected layers leading to output
+        model.add(keras.layers.GlobalAvgPool2D())
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dense(512, activation='relu'))
+        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dense(output_shape, activation='softmax'))
+
+        print(model.summary())
+
+        return model
+
     def compile_model(self, loss='categorical_crossentropy',
                       optimizer='adam',
                       metrics=['categorical_accuracy']):
@@ -112,3 +157,62 @@ class MlAlgoC:
         """
         """
         self.model = keras.models.load_model(path)
+
+
+# Code source (adapted from):
+# Hands on Machine Learning with Scikit-Learn, Keras & Tensorflow
+# by Aurélien Géron
+# Chapter 14, page 478-479
+class ResidualUnit(keras.layers.Layer):
+    """
+    """
+
+    def __init__(self, filters, strides=1, activation='relu', **kwargs):
+
+        super().__init__(**kwargs)
+        # Need to store these for get_config
+        self.filters = filters
+        self.strides = strides
+        self.activation_str = activation
+        # And this we actually use outside of __init__
+        self._activation = keras.activations.get(activation)
+
+        self.main_layers = [
+            keras.layers.Conv2D(filters, 3, strides=strides,
+                                padding='same', use_bias=False),
+            keras.layers.BatchNormalization(),
+            self._activation,
+            keras.layers.Conv2D(filters, 3, strides=1,
+                                padding='same', use_bias=False),
+            keras.layers.BatchNormalization()
+        ]
+
+        self.skip_layers = []
+        if strides > 1:
+            self.skip_layers = [
+                keras.layers.Conv2D(filters, 1, strides=strides,
+                                    padding='same', use_bias=False),
+                keras.layers.BatchNormalization()
+            ]
+
+    def call(self, inputs):
+
+        Z = inputs
+        for layer in self.main_layers:
+            Z = layer(Z)
+
+        skip_Z = inputs
+        for layer in self.skip_layers:
+            skip_Z = layer(skip_Z)
+
+        return self._activation(Z + skip_Z)
+
+    # Needed to allow saving the model
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'filters': self.filters,
+            'strides': self.strides,
+            'activation': self.activation_str,
+        })
+        return config
