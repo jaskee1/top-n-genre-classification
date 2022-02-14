fa24765096620ea58c86342278354d27e7cdc545
Jason McKeever <jasonmckeever@fakemail.com>
2022-02-14 17:33:31 -0600

    Improved compatibility with fma medium and large sets.

--- project_name/ml/c/training_script.py
+++ project_name/ml/c/training_script.py
@@ -9,7 +9,8 @@ from project_name.ml.c.ml_algo_c import MlAlgoC
 
 # Are we loading or training from a fresh state?
 LOAD_ALGO = False
-SAVE_ALGO = True
+# Save the trained algo?
+SAVE_ALGO = False
 # Are we using the resnet algo_c variant?
 USE_RESNET = True
 
@@ -18,15 +19,23 @@ if __name__ == '__main__':
     start_time = time.time()
 
     data_type = 'fma'
+    fma_set = 'medium'
 
     if len(sys.argv) > 1:
         data_type = sys.argv[1]
     if len(sys.argv) > 2:
         fma_set = sys.argv[2]
 
+    if data_type == 'fma' and (fma_set == 'medium' or fma_set == 'large'):
+        cache = False
+        batch_size = 64
+    else:
+        cache = True
+        batch_size = 32
+
     # Used to gather up all the files and associate them with splits and
     # labels.
-    loader = dl.DataLoader(data_type=data_type)
+    loader = dl.DataLoader(data_type=data_type, fma_set=fma_set)
     # Used to load features from .tfrecord files
     recorder = fr.FeatureRecorder()
 
@@ -46,9 +55,11 @@ if __name__ == '__main__':
     print(test.shape)
 
     # Create smart tensorflow dataset objects that can load the data on demand
-    training = MlAlgoC.create_dataset(training)
-    validation = MlAlgoC.create_dataset(validation)
-    test = MlAlgoC.create_dataset(test)
+    training = MlAlgoC.create_dataset(training, batch_size=batch_size,
+                                      cache=cache)
+    validation = MlAlgoC.create_dataset(validation, batch_size=batch_size,
+                                        cache=cache)
+    test = MlAlgoC.create_dataset(test, batch_size=batch_size, cache=cache)
 
     # Get previously trained algo to train it some more.
     if LOAD_ALGO:
@@ -66,12 +77,14 @@ if __name__ == '__main__':
             # Debug --- REMOVE LATER
             print('input shape:')
             print(tf.shape(elem[0]).numpy())
+            print('label shape:')
+            print(label_shape)
         ml_algo = MlAlgoC(input_shape=input_shape, output_shape=label_shape,
                           use_resnet=USE_RESNET)
 
     # Compile the model and fit the training data
     ml_algo.compile_model()
-    ml_algo.model.fit(training, epochs=50, validation_data=validation)
+    ml_algo.model.fit(training, epochs=40, validation_data=validation)
     # Evaluate performance on the test set
     ml_algo.model.evaluate(test)
 
